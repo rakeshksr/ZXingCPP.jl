af41ef3cdbf2291ae885a2d794843c0a3f9c4df9
rakeshksr <rakeshksr@users.noreply.github.com>
2025-03-01 16:39:04 +0530

    Add more formats

--- ext/ZXingCPPOpenCVExt.jl
+++ ext/ZXingCPPOpenCVExt.jl
@@ -13,10 +13,17 @@ function ZXing_CPP.ImageView(img::OpenCV.Mat{UInt8})
     c, w, h = size(img)
     fmt = if c == 1
         ZXing_CPP.libZXingCpp.ZXing_ImageFormat_Lum
-    else
+    elseif c == 2
+        ZXing_CPP.libZXingCpp.ZXing_ImageFormat_LumA
+    elseif c == 3
         ZXing_CPP.libZXingCpp.ZXing_ImageFormat_BGR
+    else
+        # c==4
+        ZXing_CPP.libZXingCpp.ZXing_ImageFormat_BGRA
     end
     return ZXing_CPP.ImageView(img, Int32(w), Int32(h), fmt)
 end
+
+ZXing_CPP.read_barcodes(img::OpenCV.Mat{UInt8}, opts::ReaderOptions=ReaderOptions()) = read_barcodes(ImageView(img), opts)
 
 end
